131e992f768ac01305f9fd0f7b76837fe7c5e9f9
Thunderbird93 <antircristian@gmail.com>
2022-11-04 16:51:36 +0100

    [add] tasks with Vue

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 const { createApp } = Vue;
 
 createApp({
+    data(){
+        return{
+            logoImg: "img/logo.png",
+            tasks: 
+            [
+                {
+                    text: 'Preparare A',
+                    done: false
+                },
+                {
+                    text: 'Preparare B',
+                    done: true
+                },
+                {
+                    text: 'Preparare C',
+                    done: false
+                }
+            ]
+        }
+    },
     mounted(){
         console.log('hi!')
     }
